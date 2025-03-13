935577f190ed4b62e2ad943a6284e7fe424b4f4d
Juan Nicolas <161638059+JuanNicolasGS@users.noreply.github.com>
2025-03-13 17:02:47 -0300

    :white_check_mark: questões finalizadas

--- Revisão/Lista AI/quest-1.py
+++ Revisão/Lista AI/quest-1.py
@@ -1,15 +1,19 @@
-# Escreva um programa que peça ao usuário para inserir 5 números e armazene-os em uma lista. Depois, calcule e exiba a soma de todos os números da lista.
+# Escreva um programa que peça ao usuário para inserir 5 números e armazene-os em uma lista. 
+# Depois, calcule e exiba a soma de todos os números da lista.
 
 # criando lista vazia
-
+lista = []
 # criando variavel soma para auxiliar a soma dos itens da lista
-
+soma = 0
 # laço for de 5 repetições
-
-# armazenando itens na lista
-
+for x in range(5):
+    # criando varivel num para armazenar na lista
+    num = int(input(f"Digite o {x + 1}° número: "))
+    # armazenando itens na lista
+    lista.append(num)
 # laço for que percorre a lista
-
-# calculando soma de itens da lista
-
+for y in lista:
+    # calculando soma de itens da lista
+    soma += y
 # exibindo soma de itens da lista
+print(f"A soma de todos os números digitados e armazenados na lista é igual a: {soma}")
--- Revisão/Lista AI/quest-2.py
+++ Revisão/Lista AI/quest-2.py
@@ -1,13 +1,17 @@
 # Peça ao usuário para inserir 10 números e armazene-os em uma lista. Depois, determine e exiba o maior e o menor número da lista.
 
 # criando lista
-
+lista = []
 # laço for de 10 repetições
-
-# armazenando na lista
-
+for x in range(10):
+    # criando variavel num para armazenar na lista
+    num = float(input(f"Digite o {x+1}º número: "))
+    # armazenando na lista
+    lista.append(num)
 # criando variaveis maior e menor para receber os valores retornados das funções min() e max()
 # | A função min() retorna o menor número dentro da lista passada como parametro para a função
 # | A função max() retorna o menor número dentro da lista passada como parametro para a função
-
+maior = max(lista)
+menor = min(lista)
 # exibindo maior e menor número da lista
+print(f"O maior valor de sua lista: {maior} \nO menor valor de sua lista: {menor}" )
