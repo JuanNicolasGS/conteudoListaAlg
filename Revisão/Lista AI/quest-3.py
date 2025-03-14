02183934bbdbc861725ad8b17eeba9008cf44570
Juan Nicolas <161638059+JuanNicolasGS@users.noreply.github.com>
2025-03-14 20:12:12 -0300

    :bulb: coment: comentdando novas questões

--- Revisão/Lista AI/quest-3.py
+++ Revisão/Lista AI/quest-3.py
@@ -1,12 +1,19 @@
 # Crie um programa que solicite 10 números ao usuário, armazene-os em uma lista e depois exiba quantos desses números são pares.
 
+# criando listas vazias
 lista = []
 pares = []
+# laço for de 10 repetições
 for x in range(10):
+    # criando variavel num para armazenar em lista
     num = int(input(f"Digite o {x+1}º número: "))
+    # armazenando num em lista
     lista.append(num)
+    # cirando condição de número para através do resto da divisão por 2
     if num % 2 == 0:
+        # adcinando num a lista pares caso num seja par
         pares.append(num)
+# exibindo lista com len para informar quantidade de itens pares
 print(f"Da lista: {lista} {len(pares)} são pares")
         
         
--- Revisão/Lista AI/quest-4.py
+++ Revisão/Lista AI/quest-4.py
@@ -1,14 +1,24 @@
 # Solicite ao usuário que informe quantos números quiser (encerre com um número negativo), 
 # armazene-os em uma lista e calcule a média dos valores inseridos (desconsiderando o número negativo que encerra a entrada).
 
+# cirando lista vazia
 lista = []
+# criando variavel auxiliar soma
 soma = 0
+# criando estrutura de reptição while True (loop infinito)
 while True:
+    # recebendo para armazenar na lisa se ele for maior igual a 0
     num = float(input("Informe um número (encerra com um número negativo): "))
+    # criando estrutura de condição para parar o loop caso resposta de num seja menor que 0
     if num < 0:
+        # quebrando estrutura de repetição
         break
+    # adicionando o número ao final da lista se ele for maior/igual a 0
     else:
         lista.append(num)
+        # fazendo soma de cada objeto na lista
         soma += num 
-media = soma / len(lista) 
+# calculando a média dos valores inseridos
+media = soma / len(lista)
+# exibindo a média dos valores inseridos na lista 
 print(f"A media dos valores inseridos é: {media}")
